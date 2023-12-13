d3ec44a44a4aba961392a72796976e252e7af55e
Adriaan <s1130674@student.hsleiden.nl>
2023-12-13 13:00:54 +0100

    Feat: Navbar Component

---
+++ src/app/nav/navbar/navbar.component.ts
@@ -0,0 +1,53 @@
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { CartService } from "../../cart/cart.service";
+import { AuthService } from "../../auth.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html'
+})
+export class NavbarComponent{
+  @ViewChild("cartView") cartView: ElementRef | undefined;
+  public showCart: boolean = false;
+  public isLoggedIn = this.authService.isLoggedIn();
+  public isAdmin = this.authService.isAdmin();
+
+
+  constructor(private http: HttpClient,
+              public cartService: CartService,
+              private router: Router,
+              private authService: AuthService) {
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    // Check if the click is outside the cart and close it
+    // @ts-ignore
+    if (!this.cartView.nativeElement.contains(event.target)) {
+      this.showCart = false;
+    }
+  }
+
+  focusOnCart() {
+
+    if (this.cartView) {
+      this.cartView.nativeElement.focus();
+      console.log("clicked")
+    }
+  }
+
+  logOut() {
+    this.authService.logOut();
+    this.router.navigateByUrl('auth/login');
+  }
+
+  toggleCart() {
+    if (this.cartService.cart.products) {
+      this.showCart = !this.showCart;
+    } else {
+      this.showCart = false;
+    }
+  }
+}

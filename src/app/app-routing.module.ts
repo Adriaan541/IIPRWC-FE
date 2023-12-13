b4611ffda4a1676896967b228fff08918ea4bde3
Adriaan <s1130674@student.hsleiden.nl>
2023-12-13 15:04:25 +0100

    Feat: Routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./account/login/login.component";
+import { RegisterComponent } from "./account/register/register.component";
+import { ProductListComponent } from "./shop/product-list/product-list.component";
+import { AccountComponent } from "./account/account/account.component";
+import { ProductPageComponent } from "./shop/product-page/product-page.component";
+import { AddEditProductComponent } from "./admin/add-edit-product/add-edit-product.component";
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth/login', component: LoginComponent},
+  { path: 'auth/register', component: RegisterComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'me', component: AccountComponent },
+  { path: 'shop', component: ProductListComponent },
+  { path: 'product/:productId', component: ProductPageComponent },
+  { path: 'addproduct', component: AddEditProductComponent },
+  { path: 'editproduct/:productId', component: AddEditProductComponent },
+  { path: 'addcategory', component: AddEditProductComponent },
+  { path: 'editcategory/:categoryId', component: AddEditProductComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
